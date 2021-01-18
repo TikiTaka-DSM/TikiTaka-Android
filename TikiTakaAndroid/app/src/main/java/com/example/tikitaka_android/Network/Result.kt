6ffa095b6d7000e9a6d207b24345ccd983566e82
minhee <jwmh15@gmail.com>
2021-01-18 17:28:58 +0900

    Create

---
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Network/Result.kt
@@ -0,0 +1,6 @@
+package com.example.tikitaka_android.Network
+
+sealed class Result<out T: Any> {
+    data class Success<out T : Any>(val data: T, val code: Int) : Result<T>()
+    data class Error(val exception: String) : Result<Nothing>()
+}
