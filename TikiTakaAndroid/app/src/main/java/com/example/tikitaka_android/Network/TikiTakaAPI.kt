1177327362e9d00c632aec3c82c94f3b0599dac1
minhee <jwmh15@gmail.com>
2021-01-18 17:32:51 +0900

    Modify Response

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Network/TikiTakaAPI.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Network/TikiTakaAPI.kt
@@ -1,13 +1,14 @@
 package com.example.tikitaka_android.Network
 
 import com.example.tikitaka_android.Sign.Data.SignUpRequest
+import com.example.tikitaka_android.Sign.Data.TokenResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TikiTakaAPI{
     @POST("/user/auth")
-    suspend fun login(@Body body: HashMap<String,String>)
+    suspend fun login(@Body body: HashMap<String,String>): Response<TokenResponse>
 
     @POST("user")
     suspend fun signUp(@Body body:SignUpRequest):Response<Unit>
