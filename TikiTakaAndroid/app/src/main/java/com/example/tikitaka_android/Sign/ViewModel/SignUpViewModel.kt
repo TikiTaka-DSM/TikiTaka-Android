2427c8b86f92c582d58afa025355974bb25525e1
minhee <jwmh15@gmail.com>
2021-01-18 17:27:23 +0900

    Create SignUpViewModel

---
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Sign/ViewModel/SignUpViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.tikitaka_android.Sign.ViewModel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tikitaka_android.Network.Result
+import com.example.tikitaka_android.Sign.Data.SignRepository
+import com.example.tikitaka_android.Sign.Data.SignUpRequest
+import kotlinx.coroutines.launch
+
+class SignUpViewModel: ViewModel() {
+    private val repository = SignRepository()
+    //val signUpSuccessLiveData: MutableLiveData<> = MutableLiveData()
+
+    fun signUp(signUpRequest: SignUpRequest){
+        viewModelScope.launch {
+            when(val result = repository.signUp(signUpRequest)){
+                is Result.Success -> {
+                    if(result.code == 201){
+                        Log.e("SignViewModel","signUp")
+                    }
+                }
+                is Result.Error -> {
+
+                }
+
+            }
+        }
+    }
+}
