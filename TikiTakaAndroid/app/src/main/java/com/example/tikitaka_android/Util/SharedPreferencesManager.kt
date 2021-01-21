6de9d4c0799a288c0903cc84f4cafd610de47fa2
minhee <jwmh15@gmail.com>
2021-01-21 23:29:11 +0900

    Modify prefs

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Util/SharedPreferencesManager.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Util/SharedPreferencesManager.kt
@@ -1,33 +1,34 @@
 package com.example.tikitaka_android.Util
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 
-class SharedPreferencesManager() {
+class SharedPreferencesManager(context: Context) {
 
-    private var sharedPreference = TikiTakaApplication.context!!.getSharedPreferences(MY_APP_PREFERENCES, MODE_PRIVATE)
+    private val pref: SharedPreferences = context.getSharedPreferences("TikiTaka-Pref",Context.MODE_PRIVATE)
 
-    var accessToken: String?
-        get() = sharedPreference.getString(SAVE_TOKEN," ")
-        set(value)= sharedPreference.edit().putString(SAVE_TOKEN,value).apply()
-
-    var refreshToken: String?
-        get() = sharedPreference.getString(SAVE_REFRESH, " ")
-        set(value) = sharedPreference.edit().putString(SAVE_REFRESH,value).apply()
-
-
-    companion object {
-        private const val MY_APP_PREFERENCES = "Yally-Android"
-        private const val SAVE_TOKEN = "accessToken"
-        private const val SAVE_REFRESH = "refreshToken"
-        private var instance: SharedPreferencesManager? = null
-
-        @Synchronized
-        fun getInstance(): SharedPreferencesManager {
-            if (instance == null) instance = SharedPreferencesManager()
-            return instance!!
-        }
-
+    fun getToken(): String {
+        return pref.getString(getKey(true),null).toString()
     }
+
+    fun saveToken(access: Boolean, token: String) {
+        pref.edit().let{
+            it.putString(getKey(access),token)
+            it.apply()
+        }
+    }
+
+    fun removeToken(){
+        pref.edit().let {
+            it.remove(getKey(true))
+            it.apply()
+        }
+    }
+
+    private fun getKey(access: Boolean): String {
+        return if(access) "accessToken" else "refreshToken"
+    }
+
+
+
 }
