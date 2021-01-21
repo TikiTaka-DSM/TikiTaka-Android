33ef62519c43267cfcdd2cf52dda9e1c5f9e9cc0
minhee <jwmh15@gmail.com>
2021-01-22 03:02:30 +0900

    Add ViewBinding

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Sign/Ui/Fragment/LoginFragment.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Sign/Ui/Fragment/LoginFragment.kt
@@ -7,34 +7,48 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.tikitaka_android.R
+import com.example.tikitaka_android.Sign.Ui.SignActivity
 import com.example.tikitaka_android.Sign.ViewModel.LoginViewModel
-import kotlinx.android.synthetic.main.activity_sign.*
+import com.example.tikitaka_android.databinding.FragmentLoginBinding
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class LoginFragment : Fragment() {
-    val viewModel: LoginViewModel by viewModels()
+    private val viewModel: LoginViewModel by viewModels()
+    private var mBinding: FragmentLoginBinding? = null
+    private val binding get() = mBinding!!
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        mBinding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         login_login_button.setOnClickListener {
-            var id = sign_id_editText.text.toString()
-            var password = sign_password_editText.text.toString()
-
-            viewModel.login(id,password)
+            loginCheck()
         }
 
-        login_notUser_button.setOnClickListener {
-
+        login_goSign_button.setOnClickListener {
+            (activity as SignActivity).setFragment("signUp")
         }
+
     }
 
+    private fun loginCheck() {
+        var id = binding.loginIdEditText.text.toString()
+        var password = binding.loginPasswordEditText.text.toString()
+
+        viewModel.login(id, password)
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
 }
--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Sign/Ui/Fragment/SignUpFragment.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Sign/Ui/Fragment/SignUpFragment.kt
@@ -8,46 +8,59 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.example.tikitaka_android.R
 import com.example.tikitaka_android.Sign.Data.SignUpRequest
+import com.example.tikitaka_android.Sign.Ui.SignActivity
 import com.example.tikitaka_android.Sign.ViewModel.SignUpViewModel
+import com.example.tikitaka_android.databinding.FragmentSignUpBinding
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
     private val viewModel: SignUpViewModel by viewModels()
+    private var mBinding: FragmentSignUpBinding? = null
+    private val binding get() = mBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        mBinding = FragmentSignUpBinding.inflate(inflater, container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         sign_signUp_button.setOnClickListener {
-            loginAfterCheck()
+            signUpCheck()
+            (activity as SignActivity).setFragment("Login")
         }
 
         sign_goLogin_button.setOnClickListener {
-
+            (activity as SignActivity).setFragment("Login")
         }
     }
 
-    private fun loginAfterCheck() {
-        if (sign_password_editText.text.toString()
-                .equals(sign_passwordCheck_editText.text.toString())
+    private fun signUpCheck() {
+        if (binding.signPasswordEditText.text.toString()
+                .equals(binding.signPasswordCheckEditText.text.toString())
         ) {
+            binding.signFailTextView.visibility = View.INVISIBLE
+
             var signUpRequest = SignUpRequest("", "", "")
 
-            signUpRequest.id = sign_id_editText.text.toString()
-            signUpRequest.name = sign_name_editText.text.toString()
-            signUpRequest.password = sign_password_editText.text.toString()
+            signUpRequest.id = binding.signIdEditText.text.toString()
+            signUpRequest.name = binding.signNameEditText.text.toString()
+            signUpRequest.password = binding.signPasswordEditText.text.toString()
 
             viewModel.signUp(signUpRequest)
         } else {
-            sign_fail_textView.text = "비밀번호가 일치하지 않습니다."
+            binding.signFailTextView.visibility = View.VISIBLE
+            binding.signFailTextView.text = "비밀번호가 일치하지 않습니다"
         }
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
     }
 
 }
