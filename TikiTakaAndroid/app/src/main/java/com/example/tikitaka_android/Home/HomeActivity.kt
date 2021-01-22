7ea26ebfc9d012ab3e82b107d9039abad2ed4a67
minhee <jwmh15@gmail.com>
2021-01-22 23:12:21 +0900

    Add bottom appbar

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Home/HomeActivity.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/Home/HomeActivity.kt
@@ -2,11 +2,55 @@ package com.example.tikitaka_android.Home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tikitaka_android.Profile.Ui.MyProfileFragment
 import com.example.tikitaka_android.R
+import com.example.tikitaka_android.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private var mBinding : ActivityHomeBinding? = null
+    private val binding = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        mBinding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(MyProfileFragment())
+        bottomNavigationItemClick()
+    }
+
+    fun bottomNavigationItemClick(){
+        binding.homeBottomAppbar.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.menu_home_list -> {
+
+                }
+
+                R.id.menu_home_chat -> {
+
+                }
+
+                R.id.menu_home_profile -> {
+                    replaceFragment(MyProfileFragment())
+                }
+
+                R.id.menu_home_addUser -> {
+
+                }
+            }
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.home_fragment, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
