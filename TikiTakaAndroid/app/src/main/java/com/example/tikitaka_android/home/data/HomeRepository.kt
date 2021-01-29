5200df883810d941d54c19f4c83015f8e9d0f9ca
minhee <jwmh15@gmail.com>
2021-01-29 23:40:51 +0900

    Modify List<Friend> -> <FriendListData>

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/data/HomeRepository.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/data/HomeRepository.kt
@@ -5,13 +5,13 @@ import com.example.tikitaka_android.network.Result
 import com.example.tikitaka_android.network.TikiTakaConnect
 
 class HomeRepository: BaseRepository() {
-    val testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTEzNzk4MzAsIm5iZiI6MTYxMTM3OTgzMCwianRpIjoiMWM4NGVjMTQtMDAwZC00MGFhLWI4MDctODlhOGU4YTZjY2Q1IiwiZXhwIjoxNjIwMDE5ODMwLCJpZGVudGl0eSI6Im1oIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.3KUVMIqr9TbAuNuJGM2vYvDm1tbNua55jAJxqC4jRMA"
+    val testToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTEzNzk4MzAsIm5iZiI6MTYxMTM3OTgzMCwianRpIjoiMWM4NGVjMTQtMDAwZC00MGFhLWI4MDctODlhOGU4YTZjY2Q1IiwiZXhwIjoxNjIwMDE5ODMwLCJpZGVudGl0eSI6Im1oIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.3KUVMIqr9TbAuNuJGM2vYvDm1tbNua55jAJxqC4jRMA"
 
     suspend fun searchFriend(id: String): Result<Unit> {
         return mappingToResult { TikiTakaConnect.createAPI().searchFriend(id) }
     }
 
-    suspend fun getFriendsList(): Result<List<Friend>>{
+    suspend fun getFriendsList(): Result<FriendListData>{
         return mappingToResult { TikiTakaConnect.createAPI().getFriendsList(testToken) }
     }
 }
