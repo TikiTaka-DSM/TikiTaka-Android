c627859d712e9ea46de51928f73505c41124701e
minhee <jwmh15@gmail.com>
2021-02-10 23:39:07 +0900

    Add Intent

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/sign/ui/SignActivity.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/sign/ui/SignActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tikitaka_android.sign.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.tikitaka_android.R
+import com.example.tikitaka_android.home.ui.HomeActivity
 import com.example.tikitaka_android.sign.ui.fragment.LoginFragment
 
 class SignActivity : AppCompatActivity() {
@@ -12,6 +14,11 @@ class SignActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign)
 
         replaceFragment(LoginFragment())
+    }
+
+    fun changeActivity(){
+        val intent = Intent(this,HomeActivity::class.java)
+        startActivity(intent)
     }
 
     fun replaceFragment(fragment: Fragment){
