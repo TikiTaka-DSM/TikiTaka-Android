c2264f203b4841a75e7d1b02290872b197872356
minhee <jwmh15@gmail.com>
2021-02-10 23:42:05 +0900

    Add result<>

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
@@ -1,17 +1,20 @@
 package com.example.tikitaka_android.chat.data
 
 import com.example.tikitaka_android.base.BaseRepository
+import com.example.tikitaka_android.home.data.RoomListData
+import com.example.tikitaka_android.network.Result
 import com.example.tikitaka_android.network.TikiTakaConnect
+import com.example.tikitaka_android.profile.data.RoomData
 import retrofit2.Response
 
 class ChatRepository: BaseRepository() {
     var testToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTEzNzk4MzAsIm5iZiI6MTYxMTM3OTgzMCwianRpIjoiMWM4NGVjMTQtMDAwZC00MGFhLWI4MDctODlhOGU4YTZjY2Q1IiwiZXhwIjoxNjIwMDE5ODMwLCJpZGVudGl0eSI6Im1oIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.3KUVMIqr9TbAuNuJGM2vYvDm1tbNua55jAJxqC4jRMA"
 
-    suspend fun joinRoom(friendId: Int) {
-        mappingToResult { TikiTakaConnect.createAPI().joinRoom(testToken,friendId) }
+    suspend fun joinRoom(friendId: Int): Result<RoomData> {
+        return mappingToResult { TikiTakaConnect.createAPI().joinRoom(testToken, friendId) }
     }
 
-    suspend fun getRoomList(){
-        mappingToResult { TikiTakaConnect.createAPI().getRoomList(testToken) }
+    suspend fun getChatList(roomID: Int): Result<ChatListData> {
+        return mappingToResult { TikiTakaConnect.createAPI().getChatList(testToken,roomID) }
     }
 }
