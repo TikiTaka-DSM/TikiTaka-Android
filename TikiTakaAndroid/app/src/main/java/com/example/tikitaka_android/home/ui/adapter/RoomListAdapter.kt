a3bef16291682f5b4131f13487c36b3b278ae1ab
minhee <jwmh15@gmail.com>
2021-02-14 23:35:05 +0900

    Add ViewChange

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/adapter/RoomListAdapter.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/adapter/RoomListAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.tikitaka_android.home.ui.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.tikitaka_android.R
+import com.example.tikitaka_android.chat.ui.ChatActivity
 import com.example.tikitaka_android.home.data.Room
 import com.example.tikitaka_android.network.TikiTakaConnect
 
@@ -23,16 +26,22 @@ class RoomListAdapter(private val roomList: List<Room>): RecyclerView.Adapter<Ro
             name.text = room.user.name
             lastChat.text = room.lastMessage
             Glide.with(itemView).load(TikiTakaConnect.s3+room.user.img).into(profileImage)
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, ChatActivity::class.java)
+                intent.putExtra("roomId",room.roomId)
+                ContextCompat.startActivity(itemView.context, intent, null)
+            }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoomListAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_room,parent,false)
 
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: RoomListAdapter.ViewHolder, position: Int)  = holder.bind(roomList[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)  = holder.bind(roomList[position])
 
     override fun getItemCount(): Int = roomList.size
 }
