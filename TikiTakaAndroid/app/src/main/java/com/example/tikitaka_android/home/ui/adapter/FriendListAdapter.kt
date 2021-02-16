434de4e3a52145fd5460157c55c8b31c7baaf45b
minhee <jwmh15@gmail.com>
2021-02-16 22:28:37 +0900

    Add ViewChange

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/adapter/FriendListAdapter.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/adapter/FriendListAdapter.kt
@@ -1,17 +1,21 @@
 package com.example.tikitaka_android.home.ui.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.tikitaka_android.R
 import com.example.tikitaka_android.chat.data.User
+import com.example.tikitaka_android.home.ui.HomeActivity
 import com.example.tikitaka_android.network.TikiTakaConnect
+import com.example.tikitaka_android.profile.ui.ProfileActivity
 
-class FriendListAdapter(private val friendList: List<User>): RecyclerView.Adapter<FriendListAdapter.ViewHolder>() {
+class FriendListAdapter(private val friendList: List<User>): RecyclerView.Adapter<FriendListAdapter.ViewHolder>(){
 
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
         val profileImage = itemView.findViewById<ImageView>(R.id.friend_profile_imageView)
@@ -20,6 +24,12 @@ class FriendListAdapter(private val friendList: List<User>): RecyclerView.Adapte
         fun bind(friend: User) {
             name.text = friend.name
             Glide.with(itemView).load(TikiTakaConnect.s3+friend.img).into(profileImage)
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context,ProfileActivity::class.java)
+                intent.putExtra("friendID", friend.id)
+                ContextCompat.startActivity(itemView.context, intent,null)
+            }
         }
     }
 
