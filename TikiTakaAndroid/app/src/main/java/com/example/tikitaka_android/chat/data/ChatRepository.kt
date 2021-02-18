2b0b3eb4fcb56e791f0c92da0ce4008e3c58108e
minhee <jwmh15@gmail.com>
2021-02-18 14:10:07 +0900

    Modify Int -> String

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
@@ -10,7 +10,7 @@ import retrofit2.Response
 class ChatRepository: BaseRepository() {
     var testToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTEzNzk4MzAsIm5iZiI6MTYxMTM3OTgzMCwianRpIjoiMWM4NGVjMTQtMDAwZC00MGFhLWI4MDctODlhOGU4YTZjY2Q1IiwiZXhwIjoxNjIwMDE5ODMwLCJpZGVudGl0eSI6Im1oIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.3KUVMIqr9TbAuNuJGM2vYvDm1tbNua55jAJxqC4jRMA"
 
-    suspend fun joinRoom(friendId: Int): Result<RoomData> {
+    suspend fun joinRoom(friendId: String): Result<RoomData> {
         return mappingToResult { TikiTakaConnect.createAPI().joinRoom(testToken, friendId) }
     }
 
