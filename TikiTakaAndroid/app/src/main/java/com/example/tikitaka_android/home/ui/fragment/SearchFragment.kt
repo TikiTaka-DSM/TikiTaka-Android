6c97c2f8e1fc2a18979262071f7f72bad2a15520
minhee <jwmh15@gmail.com>
2021-03-02 19:14:19 +0900

    Delete arguments

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/fragment/SearchFragment.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/home/ui/fragment/SearchFragment.kt
@@ -1,6 +1,8 @@
 package com.example.tikitaka_android.home.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,22 +11,15 @@ import androidx.fragment.app.viewModels
 import com.example.tikitaka_android.R
 import com.example.tikitaka_android.home.viewModel.HomeViewModel
 import com.example.tikitaka_android.databinding.FragmentSearchBinding
+import com.example.tikitaka_android.home.data.HomeRepository
+import com.example.tikitaka_android.home.ui.HomeActivity
+import com.example.tikitaka_android.network.TikiTakaConnect
+import com.example.tikitaka_android.profile.ui.ProfileActivity
 
 class SearchFragment : Fragment() {
     private var mBinding: FragmentSearchBinding? = null
     private val binding get() = mBinding!!
     private val viewModel: HomeViewModel by viewModels()
-
-    companion object {
-        const val KEY = "friendID"
-
-        fun newInstance(friendId: String) =
-                SearchFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(KEY, friendId)
-                    }
-                }
-    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +34,6 @@ class SearchFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         binding.searchButton.setOnClickListener {
-
             setFriendSearch()
         }
     }
@@ -55,11 +49,15 @@ class SearchFragment : Fragment() {
 
         viewModel.searchLiveData.observe(viewLifecycleOwner, {
             if (it) {
-                newInstance(friendId)
+                val intent = Intent(context,ProfileActivity::class.java)
+                intent.putExtra("friendID",friendId)
+                startActivity(intent)
+
             } else {
                 binding.searchCommentTextView.text = getString(R.string.notExist_id)
             }
         })
+
     }
 
     override fun onDestroyView() {
