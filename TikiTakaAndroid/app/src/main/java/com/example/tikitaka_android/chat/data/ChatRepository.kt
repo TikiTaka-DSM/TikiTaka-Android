e6cb4573afc3a8488c3daf5f51dcd6dbd0d43744
minhee <jwmh15@gmail.com>
2021-03-05 22:05:25 +0900

    Add jsonObject

--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/data/ChatRepository.kt
@@ -1,16 +1,15 @@
 package com.example.tikitaka_android.chat.data
 
 import com.example.tikitaka_android.base.BaseRepository
-import com.example.tikitaka_android.home.data.RoomListData
 import com.example.tikitaka_android.network.Result
 import com.example.tikitaka_android.network.TikiTakaConnect
 import com.example.tikitaka_android.profile.data.RoomData
-import retrofit2.Response
+import com.google.gson.JsonObject
 
 class ChatRepository: BaseRepository() {
     var testToken = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MTEzNzk4MzAsIm5iZiI6MTYxMTM3OTgzMCwianRpIjoiMWM4NGVjMTQtMDAwZC00MGFhLWI4MDctODlhOGU4YTZjY2Q1IiwiZXhwIjoxNjIwMDE5ODMwLCJpZGVudGl0eSI6Im1oIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIn0.3KUVMIqr9TbAuNuJGM2vYvDm1tbNua55jAJxqC4jRMA"
 
-    suspend fun joinRoom(friendId: String): Result<RoomData> {
+    suspend fun joinRoom(friendId: JsonObject): Result<ChatRoomData> {
         return mappingToResult { TikiTakaConnect.createAPI().joinRoom(testToken, friendId) }
     }
 
--- TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/viewModel/ChatViewModel.kt
+++ TikiTakaAndroid/app/src/main/java/com/example/tikitaka_android/chat/viewModel/ChatViewModel.kt
@@ -6,18 +6,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tikitaka_android.chat.data.ChatListData
 import com.example.tikitaka_android.chat.data.ChatRepository
+import com.example.tikitaka_android.chat.data.ChatRoomData
 import com.example.tikitaka_android.network.Result
+import com.google.gson.JsonObject
 import kotlinx.coroutines.launch
 
-class ChatViewModel: ViewModel() {
-    private var repository= ChatRepository()
+class ChatViewModel : ViewModel() {
+    private var repository = ChatRepository()
     var chatListLiveData: MutableLiveData<ChatListData> = MutableLiveData()
+    var joinRoomLiveData: MutableLiveData<Int> = MutableLiveData()
 
-    fun getChatList(roomID: Int){
+    fun getChatList(roomID: Int) {
         viewModelScope.launch {
             val result = repository.getChatList(roomID)
 
-            if(result is Result.Success){
+            if (result is Result.Success) {
                 setChatListLiveData(result)
             } else {
                 Log.e("ChatViewModel", "getChatList fail")
@@ -26,9 +29,28 @@ class ChatViewModel: ViewModel() {
     }
 
     private fun setChatListLiveData(result: Result.Success<ChatListData>) {
-        if(result.code == 200){
+        if (result.code == 200) {
             chatListLiveData.value = result.data
         }
+    }
+
+    fun joinRoom(friendId: String) {
+        val jsonObject = JsonObject()
+        jsonObject.addProperty("friend", friendId)
+
+        viewModelScope.launch {
+            val result = repository.joinRoom(jsonObject)
+
+            if (result is Result.Success) {
+                setJoinRoomLiveData(result)
+            } else if (result is Result.Error) {
+                Log.e("ChatViewModel", result.exception)
+            }
+        }
+    }
+
+    private fun setJoinRoomLiveData(result: Result<ChatRoomData>) {
+
     }
 
 }
